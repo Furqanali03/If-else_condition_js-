afe1eaf3e93b89206dbdbcdcd8e33198cd34f0fd
[Furqanali03] <[alif63503@gmail.com]>
2024-02-29 10:49:14 +0500

    JS Assignment

---
+++ app.js
@@ -0,0 +1,88 @@
+// Problem one to write a program in which using if else condition to make that the input is divisible by 3 !
+/*
+var a = +prompt("Enter your number to check ,\nit is dividible by 3 or not!")
+
+if (a % 3 === 0) {
+    alert(`Yes your number ${a} is divisible by 3`)
+} else {
+    alert(`Your number ${a} is not divisible by 3`)
+}
+*/
+
+
+
+// write  a program whether the given input is even number or an odd number 
+/*
+var a = +prompt("Enter your number to check \nit's an even or an odd!")
+
+if (a % 2 === 0) {
+    alert(`Your number ${a} is an even number!`)
+} else {
+    alert(`Your number ${a} is an odd number!`)
+}
+*/
+
+// Write an if / else statments with the following condition: if the variable age is above then 18 or not!
+/*
+var age = prompt("Kindly Enter your age!")
+
+if (age >= 18) {
+    alert("You are Old enough!")
+} else {
+    alert("You are too young!")
+}
+*/ 
+
+// Write the program to take input user name ande show if name is match to your name then write some greeting text otherwise show nothing
+/*
+var input_name = prompt("Kindly enter your name !")
+var my_name = "Furqan"
+
+if (input_name === my_name) {
+    alert("How are you Furqan? \nlong time no see!")
+}
+*/
+
+// Write a program that take as input from user about time:
+/*
+var your_time = prompt("Enter what time is it?")
+
+if (your_time >= 0 && your_time < 12) {
+    alert("Good Morning!");
+}
+else if (your_time >= 12 && your_time < 17){
+    alert("Good Afternoon!");
+}
+else if (your_time >= 17 && your_time < 21){
+    alert("Good Evening!");
+}
+else {
+    alert("Good Night!");
+}
+*/
+
+
+// Match the name 
+/*
+var my_name = prompt("What is your name?");
+var real_name = "Furqan";
+
+if (my_name === real_name) {
+    document.write(`Hello ${my_name}`)
+} else {
+    document.write("You are not Furqan!")
+}
+*/
+
+
+// Try to Fix it 
+/*
+var greeting;
+
+var hour = 13;
+if (hour < 18) {
+    greeting = "GoodDay";
+} else {
+    greeting = "GoodEvening";
+}
+*/
